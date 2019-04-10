839d4071e441ae55aa9ead576f6aec61f336fc6b
Ezequiel Regaldo <regaldo.ezequiel@gmail.com>
2019-04-10 04:18:41 -0300

    Nuevo efecto parallax

---
+++ parallax/parallax.js
@@ -0,0 +1,17 @@
+class Parallax{
+    static movement(el, coordsX, coordsY){
+        el.style['transform'] = `translate3d(${window.scrollY * coordsX}px, ${window.scrollY * coordsY}px, 0)`;
+    }
+
+    constructor(Container, CoordsY, CoordsX){
+        this.contenedor = Container;
+        this.coordsY = CoordsY;
+        this.coordsX = CoordsX || 0;
+        
+        window.addEventListener('scroll', () =>{
+            requestAnimationFrame(()=>{
+                Parallax.movement(this.contenedor, this.coordsX, this.coordsY)
+            })
+        })
+    }
+}
