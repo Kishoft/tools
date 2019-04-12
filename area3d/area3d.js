439d65dc53673a3ededa3583782e0cc3eb200925
Ezequiel Regaldo <regaldo.ezequiel@gmail.com>
2019-04-12 06:57:02 -0300

    Mejoras area 3d

--- area3d/area3d.js
+++ area3d/area3d.js
@@ -11,17 +11,14 @@ class Area3d extends HTMLElement{
             </div>
             <style>
                 :host{
-                    display: inline-flex;
-                    justify-content: center;
-                    flex-wrap: wrap;
                     position: relative;
-                    margin: 20px;
                     perspective: 400px;
                     will-change: transform;
                 }
                 :host > div{
                     width:100%;
                     height:100%;
+                    position:absolute;
                     transform-style: preserve-3d;
                     backface-visibility: hidden;
                     transition: all .2s linear;
