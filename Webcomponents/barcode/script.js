cfd3b04dcdb68de5db5c58fb7ac1c7f6875910b1
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2021-03-09 08:43:14 -0300

    Update script.js

--- Webcomponents/barcode/script.js
+++ Webcomponents/barcode/script.js
@@ -1,4 +1,12 @@
+if (!('BarcodeDetector' in window)) {
+    console.log('Barcode Detector is not supported by this browser.');
+}
+else {
+    console.log('Barcode Detector supported!');
+}
+
 let videoOutput = document.getElementById('camara')
+
 function getScreenshot(videoEl, scale) {
     scale = scale || 1;
 
@@ -12,61 +20,40 @@ function getScreenshot(videoEl, scale) {
     return image;
 }
 
-navigator.mediaDevices.getUserMedia( { video: { facingMode: { exact: "environment" } } })
-.then( mediaStream => {
 
-    if (!('BarcodeDetector' in window)) {
-        console.log('Barcode Detector is not supported by this browser.');
-    }
-    else {
-        console.log('Barcode Detector supported!');
+let barcodeDetector = new BarcodeDetector({ formats: ['code_39', 'codabar', 'ean_13'] });
 
-        
+BarcodeDetector.getSupportedFormats().then(supportedFormats => {
+    supportedFormats.forEach(format => console.log(format));
+    let supportsEan13 = supportedFormats.includes('ean_13');
 
-        BarcodeDetector.getSupportedFormats()
-            .then(supportedFormats => {
+    if (supportsEan13) {
 
-                supportedFormats.forEach(format => console.log(format));
-    
-                let supportsEan13 = supportedFormats.includes('ean_13');
-                
-                if (supportsEan13) {
-
-                    videoOutput.srcObject = mediaStream;
-                    videoOutput.play()
-    /*
-                    let imagen = document.createElement('img')
-                    imagen.src = './1200px-EAN13.svg.png';
-    */
-                    let barcodeDetector = new BarcodeDetector({ formats: ['code_39', 'codabar', 'ean_13'] });
-                    let detectado = false;
-                    do{
-                        console.log("Detectando")
-                        barcodeDetector.detect(getScreenshot(videoOutput, 1))
-                        .then(barcodes => {
-                            console.log(barcodes)
-                            barcodes.forEach(barcode => console.log(barcode.rawData));
-                            detectado = !detectado
-                        })
-                        .catch(err => {
-                          console.log(err);
-                        })
-                    }
-                    while(detectado)
-                    
-                }
-    
-                
-    
-    
-            });
-    
-    
-    
-        // create new detector
-    
     }
 
+});
 
+
+navigator.mediaDevices.getUserMedia({
+    video: {
+        facingMode: {
+            exact: "environment"
+        }
+    }
+})
+.then(mediaStream => {
+    videoOutput.srcObject = mediaStream;
+    videoOutput.play()
+    
+    setInterval(() => {
+        barcodeDetector.detect(getScreenshot(videoOutput, 1))
+            .then(barcodes => {
+                console.log(barcodes)
+                barcodes.forEach(barcode => console.log(barcode.rawData));
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, 500)
 })
 .catch(err => console.log(err));
