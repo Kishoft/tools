a286f5f9d3af16784c9e3da6458f0e4c0a6f5ec8
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2021-03-21 22:33:01 -0300

    asd

---
+++ launcher/src/index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, globalShortcut, shell } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 150,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
+    },
+    frame : false,
+    transparent : true
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('launcher.html')
+}
+
+// when app is ready register a global shortcut
+// that calls createWindow function
+app.on('ready', () => {
+    globalShortcut.register('CommandOrControl+b', createWindow)
+})
+
+// do not quit when all windows are closed
+// and continue running on background to listen
+// for shortcuts
+app.on('window-all-closed', (e) => {
+  e.preventDefault()
+  e.returnValue = false
+})
