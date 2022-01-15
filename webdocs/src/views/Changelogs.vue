2eaa0e14af4c02d404e9e2ae00506fdea1682c13
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2022-01-15 19:09:11 -0300

    Update Changelogs.vue

--- webdocs/src/views/Changelogs.vue
+++ webdocs/src/views/Changelogs.vue
@@ -1,16 +1,20 @@
 <template>
+    <Changelog timestamp="15/1/2022">
+        <li>Intersection Observer agregado.</li>
+        <li>Me sigo derritiendo con el calor que hace en Argentina.</li>
+    </Changelog>
     <Changelog timestamp="13/1/2022">
-        <li>Gran mayoría del contenido agregado al menú</li>
-        <li>Planificación de la extracción del contenido realizada</li>
-        <li>Mejoras responsivas y del menú</li>
+        <li>Gran mayoría del contenido agregado al menú.</li>
+        <li>Planificación de la extracción del contenido realizada.</li>
+        <li>Mejoras responsivas y del menú.</li>
     </Changelog>
     <Changelog timestamp="12/1/2022">
-        <li>Proyecto Iniciado</li>
-        <li>Menú MVP</li>
-        <li>Rutas MVP</li>
-        <li>Cambios</li>
-        <li>Logo improvisado</li>
-        <li>Lista de implementaciones pendientes realizada, queda mucho por realizar</li>
+        <li>Proyecto Iniciado.</li>
+        <li>Menú MVP.</li>
+        <li>Rutas MVP.</li>
+        <li>Cambios.</li>
+        <li>Logo improvisado.</li>
+        <li>Lista de implementaciones pendientes realizada, queda mucho por realizar.</li>
     </Changelog>
 </template>
 
