c87cc45f50318ea942e3b8b9306b7633e9b891ae
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2022-01-21 16:36:21 -0300

    Update index.js

--- Algorithms/CalcularVueltoBilletesOMonedas/index.js
+++ Algorithms/CalcularVueltoBilletesOMonedas/index.js
@@ -1,47 +1,50 @@
-class Cajero {
-    static montoARetirar = 9750;
-    static billetesVigentes = [
-        { 
-            monto : 1000,
-            cantidadDisponible: 1
-        },
-        { 
-            monto : 500,
-            cantidadDisponible: 5
-        },
-        { 
-            monto : 200,
-            cantidadDisponible: 5
-        },
-        { 
-            monto : 100,
-            cantidadDisponible: 5
-        },
-        { 
-            monto : 50,
-            cantidadDisponible: 5
-        }
-    ]
-    static calcularVuelto(montoDelBillete, cantidadDisponible){
-        let billetesRequeridos = Math.floor(Cajero.montoARetirar / montoDelBillete)
-        console.log(`se necesitan ${billetesRequeridos} billetes de ${montoDelBillete}`)
-        console.log(`tenemos ${cantidadDisponible} y necesitamos ${billetesRequeridos}`)
-
-        let resto = Cajero.montoARetirar % montoDelBillete;
-
-        if(cantidadDisponible < billetesRequeridos){
-            resto += montoDelBillete * (billetesRequeridos - cantidadDisponible)
-        }
-        
-        console.log(`todavía quedan por pagar $${resto}`)
-
-        Cajero.montoARetirar = resto
+let montoARetirar = 13675;
+let billetesVigentes = [
+    {
+        monto: 1000,
+        cantidadDisponible: 1
+    },
+    {
+        monto: 500,
+        cantidadDisponible: 5
+    },
+    {
+        monto: 200,
+        cantidadDisponible: 1000
+    },
+    {
+        monto: 100,
+        cantidadDisponible: 5
+    },
+    {
+        monto: 50,
+        cantidadDisponible: 5
     }
-    static calcularVueltoTotal(){
-        Cajero.billetesVigentes.forEach(billeteVigente => {
-            Cajero.calcularVuelto(billeteVigente.monto, billeteVigente.cantidadDisponible)
-        })
+]
+let informe = ""
+function calcularVuelto(valorDelBillete, billetesEnStock = 0) {
+
+    let billetesRequeridos = Math.floor(montoARetirar / valorDelBillete)
+
+    let resto = montoARetirar % valorDelBillete
+
+    informe += `Se necesitan ${billetesRequeridos} billetes de ${valorDelBillete} para retirar $${montoARetirar}, sobrarían $${resto}.\n`
+
+    if (billetesEnStock <= billetesRequeridos) {
+        informe += `Tenemos ${billetesEnStock} billetes de ${valorDelBillete} para entregar.`
+        resto += valorDelBillete * (billetesRequeridos - billetesEnStock)
     }
+
+    informe += `Por lo tanto, falta devolver ${resto}. \n`
+
+    montoARetirar = resto
+
 }
 
-Cajero.calcularVueltoTotal()
+billetesVigentes.forEach(bv => {
+    calcularVuelto(bv.monto, bv.cantidadDisponible)
+})
+
+informe += `Te quedaron ${montoARetirar} en la cuenta. No es posible retirarlos pero tranquilo, nadie te los quitará`
+
+console.log(informe)
